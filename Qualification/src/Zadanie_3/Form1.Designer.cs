a62b44062b97b5eb76fd4fa069585dd530fcf0c9
ChelIIl <63306492+ChelIIl@users.noreply.github.com>
2020-11-23 10:12:13 +0300

    Add files via upload

---
+++ Qualification/src/Zadanie_3/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Zadanie_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.пролToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сайт1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сайт2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.Status = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(600, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.пролToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            this.файлToolStripMenuItem.Click += new System.EventHandler(this.файлToolStripMenuItem_Click);
+            // 
+            // пролToolStripMenuItem
+            // 
+            this.пролToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сайт1ToolStripMenuItem,
+            this.сайт2ToolStripMenuItem});
+            this.пролToolStripMenuItem.Name = "пролToolStripMenuItem";
+            this.пролToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.пролToolStripMenuItem.Text = "Открыть";
+            // 
+            // сайт1ToolStripMenuItem
+            // 
+            this.сайт1ToolStripMenuItem.Name = "сайт1ToolStripMenuItem";
+            this.сайт1ToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.сайт1ToolStripMenuItem.Text = "Сайт 1";
+            this.сайт1ToolStripMenuItem.Click += new System.EventHandler(this.сайт1ToolStripMenuItem_Click);
+            // 
+            // сайт2ToolStripMenuItem
+            // 
+            this.сайт2ToolStripMenuItem.Name = "сайт2ToolStripMenuItem";
+            this.сайт2ToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.сайт2ToolStripMenuItem.Text = "Сайт 2 ";
+            this.сайт2ToolStripMenuItem.Click += new System.EventHandler(this.сайт2ToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Status});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 93);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(600, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // Status
+            // 
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(89, 17);
+            this.Status.Text = "Статус расчета";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 115);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Автоматизация расчетов";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem пролToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel Status;
+        private System.Windows.Forms.ToolStripMenuItem сайт1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сайт2ToolStripMenuItem;
+    }
+}
+
---
+++ Qualification/src/Zadanie_3/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie_3
+{
+    public partial class Form1 : Form
+    {
+        TextBox x_box = new TextBox();
+        TextBox y_box = new TextBox();
+        Button calculate_button = new Button();
+        Label x_label = new Label();
+        Label y_label = new Label();
+        bool flag = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Show_controls()
+        {
+            this.Height = 200;
+            this.Width = 200;
+            x_label.Parent = this;
+            x_label.Text = "Координата x: ";
+            x_label.Location = new Point(0, 30);
+            x_box.Parent = this;
+            x_box.Size = new Size(20, 20);
+            x_box.Location = new Point(100, 30);
+            y_label.Parent = this;
+            y_label.Text = "Координата y: ";
+            y_label.Location = new Point(0, 70);
+            y_box.Parent = this;
+            y_box.Size = new Size(20, 20);
+            y_box.Location = new Point(100, 70);
+            calculate_button.Parent = this;
+            calculate_button.Size = new Size(100, 30);
+            calculate_button.Location = new Point(25, 120);
+            calculate_button.Text = "Проверить";
+            calculate_button.Click += Calculate_button_Click;
+        }
+
+        private void Calculate_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double y = Convert.ToDouble(y_box.Text);
+                double x = Convert.ToDouble(x_box.Text);
+                if (flag)
+                {
+                    if ((y <= 1 && x <= 1 && x >= -1 && y >= -1))
+                    {
+                        if(y == 1 && x== 1)
+                        {
+                            Status.Text = "На границе";
+                        }
+                        else
+                            Status.Text = "Принадлежит";
+                    }
+                    else Status.Text = "Не принадлежит";
+                }
+                else
+                {
+
+                    Status.Text = "Нажат второй вариант";
+                }
+            }
+            catch (Exception ex)
+            {
+                Status.Text = ex.Message;
+            }
+        }
+
+        private void сайт1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"C:\Users\Denis\Desktop\Qualification\src\web\web1.html");
+            flag = true;
+            Show_controls();
+        }
+
+        private void сайт2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"C:\Users\Denis\Desktop\Qualification\src\web\web2.html");
+            flag = false;
+            Show_controls();
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Автор работы : Коркишко Д. А. Пкспк-217 Вариант №8");
+        }
+
+        private void файлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
